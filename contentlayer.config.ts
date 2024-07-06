8a0f291ec2052087b4823c4ce719dd371b618354
Luca <luca@ahd-creative.agency>
2024-07-06 14:33:45 +0200

    feat(core): content layer config

---
+++ contentlayer.config.ts
@@ -0,0 +1,23 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() =>({
+    name: "Post",
+    filePathPattern: `**/*.md`,
+    fields: {
+        title: { type: "string", required: true},
+        subtitle: { type: "string", required: true},
+        date: { type: "date", required: true},
+        featured_image: { type: "string", required: true},
+        categories: { type: "list", of: { type: "string"}, required: true},
+        author: { type: "string", required: true}
+    },
+    computedFields: {
+        url: {
+            type: "string",
+            resolve: (post) => `/${post._raw.flattenedPath}`
+        },
+    },
+}));
+
+
+export default makeSource({ contentDirPath: "posts", documentTypes: [Post]});
